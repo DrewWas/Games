222a26b7796e6005762f542b7545fe9a757995ef
Andrew Wasielewski <awwasielewski@gmail.com>
2022-10-04 13:09:41 -0700

    this version of breakout is bunk

--- Atari/breakout.py
+++ Atari/breakout.py
@@ -10,10 +10,8 @@ pygame.display.set_caption("breakout")
 
 # Variables
 run = True
-game_over = False
 paddle_x = randint(100,700)
 ball_x, ball_y = 700,300
-# put velo back to 6
 ball_x_velo = choice([-6,6])
 ball_y_velo = choice([-6,6])
 lives = 3
@@ -44,15 +42,14 @@ while run:
             block_color2 += 25 
 
 # Create paddle that moves back and forth
-    if game_over == False:
-        paddle = pygame.draw.rect(window, (0,138,255), pygame.Rect(paddle_x,580,150,15))
-        # Create ball
-        pygame.draw.circle(window, (255,138,0), (ball_x, ball_y), 10)
-        ball = pygame.Rect(ball_x - 7, ball_y - 7,15,15)
-        # Scoreboard 
-        pygame.font.init()
-        font = pygame.font.SysFont("Arial", 30)
-        scoreboard = window.blit(font.render("Lives: " + str(lives), False, (255,138,255)), (20,15)) 
+    paddle = pygame.draw.rect(window, (0,138,255), pygame.Rect(paddle_x,580,150,15))
+    # Create ball
+    pygame.draw.circle(window, (255,138,0), (ball_x, ball_y), 10)
+    ball = pygame.Rect(ball_x - 7, ball_y - 7,15,15)
+    # Scoreboard 
+    pygame.font.init()
+    font = pygame.font.SysFont("Arial", 30)
+    window.blit(font.render("Lives: " + str(lives), False, (255,138,255)), (20,15)) 
 
     keys = pygame.key.get_pressed() 
     if keys[pygame.K_LEFT] and paddle_x > 10:
@@ -69,40 +66,40 @@ while run:
     if ball_y < 15:
         ball_y_velo *= -1
     if paddle.colliderect(ball):
-        ball_y -= 15
+        ball_y -= 20
         ball_y_velo *= -1
 
 # If ball goes past paddle, ball resets and player loses life (3 lives)
     if ball_y > 590:
         ball_x = randint(300,700)
-        ball_y = 400 
+        ball_y = 530
         ball_y_velo *= -1
+        pygame.display.update()
         lives -= 1
+        # Game over functionality
+        if lives == 0:
+            print("\n\nyou lost nerd\n\n")
+            run = False
+# After loss of life, game pauses and resumes on paddle movement
+         
+
+
 
 # When ball hits rectangle, one of the RGB values drops and ball bounces off rectangle
     to_remove = [block for block in blocks if block.colliderect(ball)]
     for block in to_remove:
         blocks.remove(block)
-    #if ball.colliderect(block):
+        block_color += 20
+    if ball.colliderect(block):
         ball_y_velo *= -1
         ball_y -= 30 * (ball_y_velo / 6) 
 
-
-# After loss of life, game pauses and resumes on paddle movement
-
-# When ball hits paddle AND BLOCK, angle is dependent on where it hit the paddle
 
 # If this r/g/b value is below a certain #, rectangle deletes
 
 # Lives scoreboard
     pygame.font.init()
     font = pygame.font.SysFont("Arial", 30)
-
-# Game over functionality
-    if lives == 0:
-        print("\n\nyou lost nerd\n\n")
-        run = False
-
 
 # Win functionality
     if len(blocks) == 0:
@@ -128,6 +125,7 @@ OBSERVATIONS:
 """
 IMPROVEMENTS:
 * The game over functionality should be better
+* Make ball bounce relative to angle
 """
 
 
