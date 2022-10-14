7f795ca15159fe3b06518894004da20ca18ee7f2
Andrew Wasielewski <awwasielewski@gmail.com>
2022-10-14 01:47:08 -0700

    bottom squares almost sorta work

--- IOS/flappyBird/flappyBird.py
+++ IOS/flappyBird/flappyBird.py
@@ -1,5 +1,4 @@
 import pygame
-from random import choice
 
 pygame.init()
 pygame.display.init()
@@ -8,9 +7,9 @@ pygame.display.set_caption("flappy bird")
 score = 0
 birdY = 300
 birdAngle = 336
-player_velo = choice([-3,3])
+player_velo = -1.5
+tealsX = 15
 gameOver = False
-teals_list = [i * 15 for i in range(110)]
 
 def bird(birdY, birdAngle):
     bird = pygame.image.load("bird.png")
@@ -18,8 +17,8 @@ def bird(birdY, birdAngle):
     player = window.blit(bird, (100, birdY)) 
     
 
-#Background fr
 def background():
+    global tealsX
     bg = pygame.image.load("background.png")
     window.blit(bg, (0,-50))
     window.blit(bg, (550, -50))
@@ -28,9 +27,10 @@ def background():
     pygame.draw.rect(window, (184,134,11), pygame.Rect(0, 800, 800, 5))
     pygame.draw.rect(window, (32,178,170), pygame.Rect(0, 780, 800, 20))
     pygame.draw.rect(window, (101,67,33), pygame.Rect(0, 775, 800, 5))
-    for i in range(110):
         # Make these fucking squares move while game is going FITFO!!!!
-        pygame.draw.rect(window, (0,139,139), pygame.Rect(teals_list[i], 783, 9, 14))
+    for i in range(110):
+        pygame.draw.rect(window, (0,139,139), pygame.Rect(tealsX * i, 783, 9, 14))
+
 
 def collumns():
     return 1
@@ -46,7 +46,7 @@ def gameover(birdY, score):
     if birdY > 725:
         gameOver = True
         background()
-        bird(birdY, birdAngle)
+        bird(725, birdAngle)
         pygame.draw.rect(window, (207,185,151), pygame.Rect(300, 320, 200, 290), 0, 20) 
         pygame.draw.rect(window, (169,149,123), pygame.Rect(315, 335, 170, 255), 0, 20) 
         window.blit(game_over_text, (325,390))
@@ -58,7 +58,7 @@ def gameover(birdY, score):
 
 
 def main():
-    global birdY, birdAngle, player_velo
+    global birdY, birdAngle, player_velo, tealsX
     run = True
     clock = pygame.time.Clock()
     gameStart = False
@@ -68,25 +68,25 @@ def main():
             if event.type == pygame.QUIT:
                 run = False
  
-
         birdY += player_velo
+
+        if keys[pygame.K_SPACE]:
+            gameStart = True
+
         if gameStart == False:
             if birdY > 320 or birdY < 280:
                 player_velo *= -1
-       
-        if keys[pygame.K_SPACE]:
-            gameStart = True
-            player_velo = 5
 
+        if gameStart == True:
+            player_velo = 5
+            """FIX THESE NOT ALL MOVING AT SAME VELO"""
+            tealsX -= .025
 
         background()
         bird(birdY, birdAngle)
-        # this will be within (if gameStarted == False) statement
-
-
         gameover(birdY, score)
 
-        clock.tick(30)
+        clock.tick(60)
         pygame.display.update()
 
 
@@ -94,4 +94,3 @@ main()
 
 
 
-# Make background end to end (all in one function)
