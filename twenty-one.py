c6073d80fe116d796a97a1ae01b1437f4aec809c
Andrew Wasielewski <awwasielewski@gmail.com>
2023-09-19 17:46:10 -0700

    switched it up

---
+++ twenty-one.py
@@ -0,0 +1,41 @@
+# Twenty-One Rules
+
+"""
+Two players alternate turns, on which they can add 1, 2, or 3 to the
+current total. The total starts at 0 and the game ends whenever the
+total is 21 or more. The last player to add to the total loses
+
+The opponent is an ai that chooses a random number between 1 and 3
+"""
+from random import randint
+
+def play():
+    total = 0
+    turn = 1
+    while total < 21:
+        print("\nScore: " + str(total))
+        if turn > 0:
+            player_choice = ''.join(input("Add 1, 2, or 3: "))
+            if int(player_choice) not in [1,2,3]:
+                print("You must choose either 1, 2, or 3")
+                return None
+            else: 
+                total += int(player_choice)
+                turn = -turn
+
+        else:
+            x = randint(1,3)
+            print("AI adds " + str(x))
+            total += x
+            turn = -turn
+
+
+
+    print("Final score: " + str(total))
+    if turn > 0:
+        print("\nYou win!")            
+    else:
+        print("\nYou lose")            
+
+play()
+
