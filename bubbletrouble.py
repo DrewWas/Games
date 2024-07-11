11932f8c60e5bbfda6c6b63a8fbee446056e898a
Andrew Wasielewski <awwasielewski@gmail.com>
2024-07-10 22:03:54 -0400

    updates

--- bubbletrouble.py
+++ bubbletrouble.py
@@ -9,15 +9,42 @@ HEIGHT = 800
 FPS = 120
 SCREEN =  pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Bubble Trouble")
-
+LEVEL = 1
+BALL_POSITIONS = []
 
 # Colors
-black = (0,0,0)
+black = (0, 0, 0)
+blue = (0, 100, 255)
+gray = (255, 255, 255)
+
+
+class Player:
+
+    def __init__(self):
+        self.lives = 5
+        self.alive = True    
+        self.position = 700
+
+
+
+    def draw(self, position):
+        pygame.draw.rect(SCREEN, blue, (self.position, 710, 40, 90))
+        pygame.display.update()
+
+
+
+    def shoot(self, position):        
+        pygame.draw.line(SCREEN, gray, (position + 20, 710), (position + 20, 0))
+
+
 
 
 def main():
+
     running = True
     clock = pygame.time.Clock()
+    player = Player()
+
 
     while running:
         for event in pygame.event.get():
@@ -25,8 +52,23 @@ def main():
                 running = False
 
 
+        # Input functionality
+        keys = pygame.key.get_pressed()
+
+        if keys[pygame.K_SPACE]: 
+            player.shoot(player.position)
+
+        if keys[pygame.K_LEFT] and player.position > 5: 
+            player.position -= 8  
+
+        if keys[pygame.K_RIGHT] and player.position < 1349: 
+            player.position += 8
+
+
+        player.draw(player.position)
+
+        clock.tick(FPS)
         SCREEN.fill(black)
-        pygame.display.flip()
 
 
     pygame.quit()
@@ -34,4 +76,8 @@ def main():
 
 if __name__ == "__main__":
     main()
+
+
+
+
 
