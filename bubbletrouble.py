2bfc85b5089bec95ad3b2ce02be44cc43434dea9
Andrew Wasielewski <awwasielewski@gmail.com>
2024-07-10 23:22:48 -0400

    checkpoint

--- bubbletrouble.py
+++ bubbletrouble.py
@@ -1,5 +1,28 @@
 import pygame
 from random import randint
+from time import sleep
+
+
+"""
+TODO:
+* Homescreen and gameover screen
+
+* Arrow shoots up gradually
+
+* Nicer graphics and backgrounds
+
+* Player and ball collision detection
+
+* Arrow and ball collision detection
+
+* Ball split
+
+* Weird skipping/lagging
+"""
+
+
+
+
 
 # Setup
 pygame.init()
@@ -9,8 +32,11 @@ HEIGHT = 800
 FPS = 120
 SCREEN =  pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Bubble Trouble")
-LEVEL = 1
-BALL_POSITIONS = []
+GAMEOVER = True
+
+balls = []
+
+
 
 # Colors
 black = (0, 0, 0)
@@ -20,21 +46,67 @@ gray = (255, 255, 255)
 
 class Player:
 
+
     def __init__(self):
         self.lives = 5
         self.alive = True    
         self.position = 700
+        self.line = None
 
 
 
     def draw(self, position):
         pygame.draw.rect(SCREEN, blue, (self.position, 710, 40, 90))
-        pygame.display.update()
 
 
 
     def shoot(self, position):        
-        pygame.draw.line(SCREEN, gray, (position + 20, 710), (position + 20, 0))
+        self.line = pygame.draw.line(SCREEN, gray, (position + 20, 710), (position + 20, 0))
+
+
+
+
+class Balls:
+
+    def __init__(self, layers, bouncy, x_vel, y_vel):
+        self.layers = layers
+        self.bouncy = bouncy
+        self.radius = 24 
+        self.x_pos = randint(100,1300) 
+        self.y_pos = 100 
+        self.x_vel = x_vel 
+        self.y_vel = y_vel  
+        self.x_acc = 0
+        self.y_acc = 9.81 / 100 
+
+
+
+    def draw(self):
+        self.x_pos += self.x_vel
+        self.y_vel += self.y_acc
+        self.y_pos += self.y_vel
+
+        if self.y_pos >= (HEIGHT - self.radius):
+            self.y_vel *= -1
+
+        if self.x_pos >= (WIDTH - self.radius):
+            self.x_vel *= -1
+
+        if self.x_pos <= (self.radius):
+            self.x_vel *= -1
+
+
+        pygame.draw.circle(SCREEN, blue, (self.x_pos, self.y_pos), self.radius)
+
+
+
+    def split(self):
+        global balls
+
+        new1 = Balls(2,2, self.x_vel * -1, abs(self.y_vel) + 2)
+        new2 = Balls(2,2, self.x_vel, abs(self.y_vel) + 2)
+        balls.extend([new1, new2])
+        
 
 
 
@@ -45,8 +117,11 @@ def main():
     clock = pygame.time.Clock()
     player = Player()
 
+    balls.append(Balls(2, 2, 3, 2))
 
     while running:
+
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
@@ -67,11 +142,29 @@ def main():
 
         player.draw(player.position)
 
+
+
+        # TESTING!!
+        if keys[pygame.K_SPACE]:
+            balls[0].split() 
+
+
+        print(len(balls))
+        for ball in balls:
+            ball.draw()
+
+        #test_ball.draw()
+
+        # DONE TESTING
+
+        pygame.display.update()
+
         clock.tick(FPS)
         SCREEN.fill(black)
 
 
     pygame.quit()
+
 
 
 if __name__ == "__main__":
