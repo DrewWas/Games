cfb248eb6c65173ffe2d9fbf3402f3374f55ab80
Andrew Wasielewski <awwasielewski@gmail.com>
2024-07-25 21:15:52 -0400

    paddles

--- pong/pong.py
+++ pong/pong.py
@@ -20,7 +20,9 @@ class Game:
         #self.ball = Ball()
         pygame.init()
         pygame.font.init()
-        MY_FONT = pygame.font.SysFont('Futura', 20)
+        MY_FONT = pygame.font.Font('fontpixel.ttf', 70)
+        self.player1_pos = [10, randint(200, 500)] 
+        self.player2_pos = [1470, randint(200, 500)] 
         self.score = Score(MY_FONT)
 
     def start(self):
@@ -29,6 +31,9 @@ class Game:
         self.clock = pygame.time.Clock()
         self.SCREEN = pygame.display.set_mode((HEIGHT, WIDTH))
         pygame.display.set_caption("python pong")
+        
+        player1 = Paddle(self.player1_pos) 
+        player2 = Paddle(self.player2_pos) 
 
         # main event loop
         while self.running:
@@ -36,11 +41,32 @@ class Game:
                 if event.type == pygame.QUIT:
                     self.running = False
 
+            
+            keys = pygame.key.get_pressed()
+
+            if keys[pygame.K_w] and self.player1_pos[1] > 5:
+                self.player1_pos[1] -= 7 
+                player1.update(BLUE, self.player1_pos)
+
+            if keys[pygame.K_s] and self.player1_pos[1] < 620:
+                self.player1_pos[1] += 7 
+                player1.update(BLUE, self.player1_pos)
+
+            if keys[pygame.K_UP] and self.player2_pos[1] > 5:
+                self.player2_pos[1] -= 7 
+                player2.update(RED, self.player2_pos)
+
+            if keys[pygame.K_DOWN] and self.player2_pos[1] < 620:
+                self.player2_pos[1] += 7 
+                player2.update(RED, self.player2_pos)
+
+            player1.update(BLUE, self.player1_pos)
+            player2.update(RED, self.player2_pos)
+
 
 
             self.score.draw_score(self.player1_score, self.player2_score)
-            self.player1_score += 1
-            self.player2_score += 1
+
 
             pygame.display.update()
             self.clock.tick(FPS)
@@ -55,8 +81,16 @@ class Game:
 
 
 
-#class Paddle:
+class Paddle:
 
+    def __init__(self, player_pos):
+        self.player_pos = player_pos
+
+    def update(self, color, pos):
+        self.player_pos = pos
+        pygame.draw.rect(game.SCREEN, color, (self.player_pos[0], self.player_pos[1], 20, 120))
+
+        print(self.player_pos)
 
 
 class Score:
@@ -70,10 +104,8 @@ class Score:
         player1_surface= self.font.render(str(player1_score), False, WHITE)
         player2_surface = self.font.render(str(player2_score), False, WHITE)
 
-        game.SCREEN.blit(player1_surface, (100,100))
-        game.SCREEN.blit(player2_surface, (200,200))
-
-
+        game.SCREEN.blit(player1_surface, (350,50))
+        game.SCREEN.blit(player2_surface, (1050,50))
 
 
 
