3122b8e630702922f70acf5373b671cbf98e777c
Andrew Wasielewski <awwasielewski@gmail.com>
2024-08-08 02:45:17 -0400

    actually random starting positions

--- pong/pong.cpp
+++ pong/pong.cpp
@@ -1,5 +1,7 @@
 #include <SFML/Graphics.hpp>
 #include <iostream>
+#include <random>
+#include <ctime>
 
 int main() {
 
@@ -8,16 +10,23 @@ int main() {
     sf::RenderWindow window(sf::VideoMode(1500,750), "Pong C++");
     window.setFramerateLimit(60);
 
+    // Randomness for initial paddle positions
+    std::default_random_engine generator(static_cast<unsigned>(std::time(nullptr)));
+    std::uniform_int_distribution<int> distribution(100,600);
+
+    int player1_initial = distribution(generator);
+    int player2_initial = distribution(generator);
+
 
     // Create players  
     sf::RectangleShape player1;
-    sf::Vector2f player1_pos(10, 120);
+    sf::Vector2f player1_pos(10, player1_initial);
     player1.setPosition(player1_pos);
     player1.setSize(sf::Vector2f(20,120));
     player1.setFillColor(sf::Color::Red);
 
     sf::RectangleShape player2;
-    sf::Vector2f player2_pos(1470, 120);
+    sf::Vector2f player2_pos(1470, player2_initial);
     player2.setPosition(player2_pos);
     player2.setSize(sf::Vector2f(20,120));
     player2.setFillColor(sf::Color::Blue);
