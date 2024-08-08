5024df263fc37c3e18af061e3240fe6adf23b68b
Andrew Wasielewski <awwasielewski@gmail.com>
2024-08-08 03:41:07 -0400

    ball wall collision issues still there

--- pong/pong.cpp
+++ pong/pong.cpp
@@ -10,6 +10,7 @@ int main() {
     sf::RenderWindow window(sf::VideoMode(1500,750), "Pong C++");
     window.setFramerateLimit(60);
 
+
     // Randomness for initial paddle positions
     std::default_random_engine generator(static_cast<unsigned>(std::time(nullptr)));
     std::uniform_int_distribution<int> distribution(100,600);
@@ -29,7 +30,20 @@ int main() {
     sf::Vector2f player2_pos(1470, player2_initial);
     player2.setPosition(player2_pos);
     player2.setSize(sf::Vector2f(20,120));
-    player2.setFillColor(sf::Color::Blue);
+    player2.setFillColor(sf::Color(0, 138, 255)); // (0, 138, 255) is the RGB for a cleaner shade of blue
+
+    // Create ball
+    int ball_x_velo = 12;      // Change to be based on difficulty, which starts at 12 (this is hardcoded temporarily)
+    int ball_y_velo = 12;
+    sf::CircleShape ball(12);
+    sf::Vector2f ball_pos(750, 375);
+    ball.setPosition(ball_pos);
+    
+
+
+    // Create scores and print to screen
+    int player1_score = 0;
+    int player2_score = 0;
 
 
     // Main game loop
@@ -43,6 +57,12 @@ int main() {
 
 
         }
+
+        // Ball movement and logic
+        ball_pos.x += ball_x_velo;
+        ball_pos.y += ball_y_velo;
+
+
 
         // Player 1 movements
         if (sf::Keyboard::isKeyPressed(sf::Keyboard::S) && player1_pos.y < 620) { 
@@ -66,9 +86,48 @@ int main() {
             player2.setPosition(player2_pos);
         }
 
+
+
+        std::cout << "Player 1 y: " << player1_pos.y << "\n";
+        std::cout << "Player 2 y: " << player2_pos.y << "\n";
+        // Ball paddle/wall collision logic
+        if (ball_pos.x < 30) {
+
+            if (player1_pos.y - 10 < ball_pos.y < player1_pos.y + 120 + 10) {
+                ball_pos.x += 10;
+                ball_x_velo *= -1;
+            }
+
+            else if (ball_pos.x < -50) {
+                ball_pos.x = 375;
+            }
+        }
+    
+        else if (ball_pos.x > 1460) {
+
+            if (player2_pos.y - 10 < ball_pos.y < player2_pos.y + 120 + 10) {
+                ball_pos.x -= 10;
+                ball_x_velo *= -1;
+            }
+
+            else if (ball_pos.x > 1550) {
+                ball_pos.x = 375;
+            }
+        }
+
+        if (ball_pos.y > 745 || ball_pos.y < 5) {
+            ball_y_velo *= -1;
+        }
+
+        ball.setPosition(ball_pos);
+
+
+
+
         window.clear(sf::Color::Black);
         window.draw(player1);
         window.draw(player2);
+        window.draw(ball);
         window.display();
 
     }
