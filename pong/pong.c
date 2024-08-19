2e4abaf3a0151d9ac0c0e28141d89fb678c3ce5e
Andrew Wasielewski <awwasielewski@gmail.com>
2024-08-18 20:55:01 -0400

    started on ball

--- pong/pong.c
+++ pong/pong.c
@@ -3,6 +3,20 @@
 #include <stdio.h>
 
 
+void drawBall(SDL_Renderer *renderer, int ball_x, int ball_y, int radius) {
+    for (int x = -radius; x <= radius; x++) {
+        for (int y = -radius; y <= radius; y++) {
+            if ((x*x) + (y*y) < (radius * radius)) {
+                SDL_RenderDrawPoint(renderer, ball_x + x, ball_y + y);
+            }
+        }
+    }
+
+
+
+}
+
+
 int main(int argc, char* argv[]) {
 
     // Setup and Constants
@@ -24,6 +38,8 @@ int main(int argc, char* argv[]) {
     int player2_y = 300;  // Make random
     int ball_x = 750;
     int ball_y = 375;
+    int ball_x_velo = 13;
+    int ball_y_velo = 13;
 
     // Player 1
     SDL_Rect player1;
@@ -54,9 +70,20 @@ int main(int argc, char* argv[]) {
 
     // Font & Text Setup
     TTF_Init();
-    TTF_Font* pixelFont = TTF_OpenFont("Helpers/fontpixel.ttf", 70);
-    SDL_Surface* p1ScoreSurface = TTF_RenderText_Solid(pixelFont, " HELLO!!", WHITE);
+    TTF_Font* pixelFont = TTF_OpenFont("Helpers/fontpixel.ttf", 10);
+
+    SDL_Rect destR = { 10, 10, 10, 10}; // test
+
+
+    char score_text[2];
+    sprintf(score_text, "%d", player1_score);
+
+    //SDL_Surface* p1ScoreSurface = TTF_RenderText_Solid(pixelFont, "HELLO!!!", WHITE);
+    SDL_Surface* p1ScoreSurface = TTF_RenderText_Solid(pixelFont, score_text, WHITE);
     SDL_Texture* texture = SDL_CreateTextureFromSurface(renderer, p1ScoreSurface);
+
+
+    TTF_SizeText(pixelFont, score_text, &destR.w, &destR.h); // test 
 
 
     int keyState[SDL_NUM_SCANCODES] = {0};
@@ -76,6 +103,7 @@ int main(int argc, char* argv[]) {
 
         if (state[SDL_SCANCODE_W] && player1.y > 10) {
             player1.y -= 15; 
+            //player1_score += 1; // TESTING!!
         }
 
         if (state[SDL_SCANCODE_S] && player1.y < 620) {
@@ -90,7 +118,32 @@ int main(int argc, char* argv[]) {
             player2.y += 15; 
         }
 
+        // Update ball position (add if conditions)
+        ball_x += ball_x_velo;
+        ball_y -= ball_y_velo;
 
+        if (ball_x < 15 || ball_x > 1485) {
+            // check if player paddle is in the way and if not reset
+            if ((ball_x < 15) && (ball_y > player1.y - 10) && (ball_y < player1.y + 130)) {
+                ball_x_velo *= -1;
+            }
+            else if ((ball_x > 1485) && (ball_y > player2.y - 10) && (ball_y < player2.y + 130)) {
+                ball_x_velo *= -1;
+            }
+            else {
+                ball_x = 750;
+                ball_y = 375;
+            }
+        }
+
+
+        if (ball_y < 15 || ball_y > 735) {
+            ball_y_velo *= -1;
+        }
+
+
+        // DEBUGGING
+        //printf("%s", player1_score);
         
 
         // Draw background 
@@ -99,10 +152,12 @@ int main(int argc, char* argv[]) {
 
 
         // Draw Scores 
-        SDL_RenderCopy(renderer, texture, NULL, NULL);
-        //SDL_RenderPresent(renderer);   (Why is this needed)??
+        //SDL_RenderCopy(renderer, texture, NULL, NULL);
+        //SDL_RenderPresent(renderer);  (why is this needed?? It just fucks shit up)
 
         // Draw Ball
+        SDL_SetRenderDrawColor(renderer, 255, 255, 255, 0);
+        drawBall(renderer, ball_x, ball_y, 15); 
 
 
         // Draw Player1
